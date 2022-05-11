df9b248b8c4c945d537ddc9e733074f4b2fca22e
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-11 17:58:28 +0530

    [shravanthi] -Added Binary Search Tree with Insert Method

---
+++ BinarySrchTree/BST_Methods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySrchTree
+{
+    public class BST_Methods<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BST_Methods<T> LeftTree { get; set; }
+        public BST_Methods<T> RightTree { get; set; }
+        public BST_Methods(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = LeftTree;
+            this.RightTree = RightTree;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BST_Methods<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BST_Methods<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+        
+    }
+}
---
+++ BinarySrchTree/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinarySrchTree;
+
+Console.WriteLine("Binary Search Tree!");
+
+BST_Methods<int> bs1 = new(56);
+bs1.Insert(30);
+bs1.Insert(70);
+bs1.Display();
